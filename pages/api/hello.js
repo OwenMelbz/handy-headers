133be28250e8698dc5cb5600b028c5ab5f0b560c
Owen Melbourne <owen@stormcreative.co.uk>
2021-02-10 15:49:12 +0000

    🚀 Feature - initial code

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,79 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import fetch from 'node-fetch'
+import { data_get } from 'laravel-js-helpers'
+import moment from 'moment'
 
-export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
+
+export default async (req, res) => {
+
+  const response = (await fetch(req.query.url))
+  const headers = JSON.parse(JSON.stringify(response.headers.raw()))
+
+  let age = data_get(headers, 'age.0')
+
+  if (age) {
+    age = moment.duration(age * 1000).locale('en').humanize()
+  }
+
+  let last_modified = data_get(headers, 'last-modified.0')
+
+  if (last_modified) {
+    last_modified = moment(last_modified).calendar()
+  }
+
+  let cache_control = data_get(headers, 'cache-control', []) || []
+  let max_age = null
+
+  if (cache_control.length) {
+    cache_control.forEach(entry => {
+      const [header, value] = entry.split('=')
+      if (header === 'max-age') {
+        max_age = moment.duration(value * 1000).locale('en').humanize()
+      }
+    })
+  }
+
+  let expires = data_get(headers, 'expires.0')
+
+  if (expires) {
+    expires = moment(expires).calendar(null, {
+      sameElse: 'LLLL'
+    })
+  }
+
+  let encoding = (data_get(headers, 'content-encoding', []) || []).join(', ') || null
+
+  const contentType = data_get(headers, 'content-type.0', 'text/undefined') || ''
+
+  let body = null
+
+  if (contentType.indexOf('text') !== -1) {
+    body = await response.text()
+  } else {
+    const base64 = (await response.buffer()).toString('base64')
+    body = `data:${contentType};base64,${base64}`
+  }
+
+  const result = {
+    display: {
+      'Status': `${response.status} - ${response.statusText}`,
+      'Redirected To': response.redirected ? response.url : null,
+      'Authentication': data_get(headers, 'www-authenticate.0') || null,
+      'Cache Control': cache_control.join(', '),
+      'CF Cache': data_get(headers, 'cf-cache-status.0') || null,
+      'Last Modified': last_modified,
+      'Age': age,
+      'Max Age': max_age,
+      'Expires': expires,
+      'Encoding': encoding,
+      'Content Type': contentType,
+    },
+    log: {
+      headers: headers,
+      body,
+    },
+  }
+
+  res.status(200).json(result)
 }
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,74 @@
+import axios from 'axios';
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { useState } from 'react';
+import 'console.image'
+
+const defaultUrl = 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png'
 
 export default function Home() {
+
+    const [url, setUrl] = useState(defaultUrl)
+    const [result, setResult] = useState(null)
+
+    const search = async (e) => {
+        e.preventDefault();
+        const { data } = await axios.get(`/api/hello?url=${url}`)
+
+        setResult(data)
+
+        console.table(data.log.headers)
+        console.image(data.log.body)
+    }
+
   return (
-    <div className={styles.container}>
+    <div className={'text-white bg-purple-700 antialiased min-h-screen flex items-center'}>
       <Head>
-        <title>Create Next App</title>
+        <title>Handy Headers</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <main className={'p-6 max-w-4xl mx-auto'} style={{ width: '100%'}}>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+          <h1 title={'Just the ones we need to see.'} className={'font-bold text-center uppercase text-xs'}>
+              Handy Headers
+          </h1>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          <form action="/api/hello" method={'get'} onSubmit={search} className={'mt-10 block w-full border-2 border-white rounded-full flex items-center overflow-hidden p-4'}>
+              <div className={'flex-1 pr-5'}>
+                  <input placeholder={`e.g. ${defaultUrl}`} className={'text-center bg-transparent h-full w-full focus:outline-none'} name={'url'} type="url" onChange={e => setUrl(e.target.value)} value={ url }/>
+              </div>
+              <div className={'w-10 h-10'}>
+                  <button className={'w-full h-full p-1 focus:outline-none text-white hover:text-purple-200'} disabled={!url} type='submit'>
+                      <svg className={'w-full h-full'} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M21.172 24l-7.387-7.387c-1.388.874-3.024 1.387-4.785 1.387-4.971 0-9-4.029-9-9s4.029-9 9-9 9 4.029 9 9c0 1.761-.514 3.398-1.387 4.785l7.387 7.387-2.828 2.828zm-12.172-8c3.859 0 7-3.14 7-7s-3.141-7-7-7-7 3.14-7 7 3.141 7 7 7z"/></svg>
+                  </button>
+              </div>
+          </form>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+          { result && <div className={'mt-10 mx-auto max-w-lg'}>
+              <table>
+                  <tbody>
+                  { Object.keys(result.display).filter(key => result.display[key]).map(key => {
+                      return <tr key={key}>
+                          <th className={'text-left'}>
+                              <span className={'px-4'}>
+                                  { key }:
+                              </span>
+                          </th>
+                          <td>
+                              <span className={'px-4'}>
+                                  { result.display[key] }
+                              </span>
+                          </td>
+                      </tr>
+                  })}
+                  </tbody>
+              </table>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+              <div className={'text-xs text-center mt-20 px-4'}>
+                  (check dev console for more data)
+              </div>
+          </div>}
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   )
 }
